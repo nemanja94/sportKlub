fd1f1e3eb5ee1653649b45c188b736fff8946243
Nemanja Andric <nemanja@localhost.localdomain>
2018-10-25 17:22:07 +0200

    Dodate komponente:   *add-event   *profile

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,18 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from '../pages/login/login.component';
+
+import { LoginComponent } from "../pages/login/login.component";
 import { RegisterComponent } from '../pages/register/register.component';
 import { HomeComponent } from '../pages/home/home.component';
-import { CommonModule } from '@angular/common';
+import { AddEventComponent } from '../pages/add-event/add-event.component';
+import { ProfileComponent } from '../pages/profile/profile.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'add-event', component: AddEventComponent },
+  { path: 'profile', component: ProfileComponent }
 ];
 
 @NgModule({
   imports: [CommonModule, RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AddEventComponent } from './pages/add-event/add-event.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,8 @@ import { HomeComponent } from './pages/home/home.component';
     LoginComponent,
     RegisterComponent,
     HomeComponent,
+    AddEventComponent,
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
