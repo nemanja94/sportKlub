0438a45431fbe746a99e4f87a6cd42b3813ecc16
Nemanja Andric <nemanja@localhost.localdomain>
2018-10-29 18:18:35 +0100

    Zavrsen kod koji salje podatke o registraciji backendu. Ostalo samo da se sredi antibot provera (checkbox)!!!

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Headers, Http } from '@angular/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  public registerForm = new FormGroup({
+    firstName: new FormControl(),
+    lastName: new FormControl(),
+    username: new FormControl(),
+    email: new FormControl(),
+    password: new FormControl(),
+    antiBot: new FormControl()
+  })
 
-  ngOnInit() {
+  constructor(private _http: Http, private _router: Router) { }
+
+  public register() {
+    // tslint:disable-next-line:max-line-length
+    const data = 'firstName=' + this.registerForm.value.firstName +
+      '&lastName=' + this.registerForm.value.lastName +
+      '&email=' + this.registerForm.value.email +
+      '&username=' + this.registerForm.value.username +
+      '&password=' + this.registerForm.value.password;
+
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+
+    this._http.post('server', data, { headers: headers }).subscribe(result => {
+      const obj = JSON.parse(result['_body']);
+      localStorage.setItem('token', obj.token);
+      localStorage.removeItem('token');
+      this._router.navigateByUrl('/login');
+    },
+      err => {
+        const obj = JSON.parse(err._body);
+        const element = <HTMLElement>(
+          document.getElementsByClassName('alert')[0]
+        );
+        element.style.display = 'block';
+        element.innerHTML = obj.error.split('\\r\\n').join('<br/>').split('\"').join('');
+      })
   }
 
 }
